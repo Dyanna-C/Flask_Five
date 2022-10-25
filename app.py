9e3f60f1dbff66d311088fdab99ab5f8a9b78af0
Dyanna <dyanna.person@gmail.com>
2022-10-25 00:41:30 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+#Create an insatnce of the Flask class and give it the variable name "app"
+app = Flask(__name__)
+
+#Create a rooute usin the @app.route to trigger function base don endpoint
+@app.route('/')
+def index():
+    return 'Hello this is the index route!'
+
+
+#add another route
+@app.route('/posts')
+def posts():
+    return 'Posts will eventually be on this page.'
